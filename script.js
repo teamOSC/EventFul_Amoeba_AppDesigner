f3b8451a3d3950a4d6c1bdf356c7dcf708d100d7
sauravtom <stomatrix@gmail.com>
2014-02-14 12:57:37 +0530

    <3 roboto font

--- script.js
+++ script.js
@@ -2,10 +2,14 @@
    Globals          
 =============*/
 $('.content>*').hide();
-$('.content>.register').show();
+$('.content>.events').show();
 
 $('.button').click(function () {
     $(".sidebar").toggle();
+});
+
+$('#publish').click( function(){
+
 });
 
 /*Sexy string formatting hack in jquery*/
@@ -29,9 +33,6 @@ function handleFileSelect(evt) {
       return function(e) {
         img.attr("src",e.target.result);
         img.attr("title",theFile.name);
-        //var span = document.createElement('span');
-        //span.innerHTML = ['<img src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
-        //document.getElementById('img_placeholder').insertBefore(span, null);
       };
   })(f);
   reader.readAsDataURL(f);
@@ -69,10 +70,10 @@ $('.events>button').click(function () {
   template_tab='<input id="tab{0}" match="slide{1}" class="invisible tab" value ="Tab {2}">'.f(count,count,count);
 
   template_slide='<div class="slides" id="slide{0}">\
-          <button class="floatright remove hint--right" data-hint="Delete tab #{0}" id="{0}">-</button>\
-          <img class="img" src="http://placehold.it/450x250&text=Tab {0}">\
+          <button class="addrem floatright remove hint--right" data-hint="Delete Tab #{0}" id="{0}">-</button>\
+          <img class="img" src="http://placehold.it/450x250&text=Tab #{0}">\
           <input type="file" id="img_upload_input" name="data[]"/>\
-          <textarea class="invisible" placeholder="Description for tab {0}"></textarea>\
+          <textarea class="invisible" placeholder="Description for Tab #{0}"></textarea>\
         </div>'.f(count);
 
   $('.band').append(template_tab);
@@ -107,9 +108,9 @@ Register page
 $('.register>button').click(function () {
   var count = $(".regfield").length+1;
   var template_reg = '<div class="field regfield">\
-        <input class="invisible" placeholder="field {1}">\
-        <input placeholder="input box {2}">\
-      </div>'.f(count,count,count);
+        <input class="invisible" placeholder="Field #{0}">\
+        <input placeholder="Input box #{0}">\
+      </div>'.f(count);
   $(".register").append(template_reg);
 });
 
@@ -119,9 +120,9 @@ Contact page
 $('.contact>button').click(function () {
   var count = $(".confield").length +1;
   var template_con = '<div class="field confield">\
-        <input id="title" class="invisible" placeholder="title{0}">\
-        <input class="invisible" placeholder="name {0}">\
-        <input class="invisible" placeholder="number {0}">\
+        <input id="title" class="invisible" placeholder="Title #{0}">\
+        <input class="invisible" placeholder="Name #{0}">\
+        <input class="invisible" placeholder="Number #{0}">\
       </div>'.f(count);
   $(".contact").append(template_con);
 });
